2660d2d62edfd74bd4aee86312bc8d3e004bbcff
Friftycode <arthur.leonard.thomassen@gmail.com>
2024-09-27 12:37:31 +0200

    feat: added folder page and pages HomePage and FeetPage as .tsx and .css files.

---
+++ feet/src/pages/FeetPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./FeetPage.css";
+
+const FeetPage = (): JSX.Element => {
+    return <div>FEET page</div>;
+};
+
+export default FeetPage;
---
+++ feet/src/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./HomePage.css";
+
+const HomePage = (): JSX.Element => {
+    return <div>Home page</div>;
+};
+
+export default HomePage;
