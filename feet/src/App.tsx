c10e8e74223a4b0d14184795ec2e2a1a15117094
Friftycode <arthur.leonard.thomassen@gmail.com>
2024-09-27 13:42:57 +0200

    feat: added react route.

--- feet/src/App.tsx
+++ feet/src/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import {Routes, Route} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import FeetPage from "./pages/FeetPage";
 import "./App.css";
 
 const App = (): JSX.Element => {
-  return <div>App</div>;
+    return (
+        <Routes>
+            <Route path="/" element={<HomePage/>}/>
+            <Route path="/feet" element={<FeetPage/>}/>
+        </Routes>
+    );
 };
 
 export default App;
