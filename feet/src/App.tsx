034e2633f5236553cee4d627dbbe1fabb9360c05
NghiNg <60237320+NghiNg@users.noreply.github.com>
2024-10-04 09:54:17 +0200

    chore: remove accidental duplicate button (#25)

--- feet/src/App.tsx
+++ feet/src/App.tsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
 import LanguageSelect from './components/SwitchLanguageButton';
-import DarkmodeToggle from './components/DarkmodeToggle';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/homepage/HomePage';
@@ -11,7 +10,6 @@ const App = (): JSX.Element => {
     <>
       <Header />
       <LanguageSelect />
-      <DarkmodeToggle />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/feet" element={<FeetPage />} />
