4fcac66c2c57e8ec9e89a160ef00a4e0192010e1
NghiNg <60237320+NghiNg@users.noreply.github.com>
2024-10-05 10:08:02 +0200

    feat: Add panel sheet functionality (#31)

--- feet/src/pages/feetpage/ExportForm.tsx
+++ feet/src/pages/feetpage/ExportForm.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react';
+import { FC, FormEventHandler, useState } from 'react';
 import { utils, write } from 'xlsx';
 import FileSaver from 'file-saver';
 
@@ -7,18 +7,30 @@ import { useLanguageContext } from '../../utils/LanguageProvider';
 import { Root } from '../../interfaces/jsonDataInterface';
 import styles from './ExportForm.module.less';
 import GenericButton from '../../components/GenericButton';
+import { mapPanelToExcel } from '../../mappers/panel-utils';
 
 const ExportForm: FC<{ data?: Root }> = ({ data }) => {
   const { translate } = useLanguageContext();
   const [fileName, setFileName] = useState<string>(
     translate('export-filename-placeholder'),
   );
+  const [panel, setPanel] = useState<boolean>(false);
 
-  const exportToExcel: React.FormEventHandler<HTMLFormElement> = (e) => {
+  const exportToExcel: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
+    if (data === undefined) return;
     const worksheet = utils.json_to_sheet([data]);
 
     const workbook = utils.book_new();
+    if (panel) {
+      utils.book_append_sheet(
+        workbook,
+        utils.json_to_sheet(
+          data.system.panels.map((panel) => mapPanelToExcel(panel)),
+        ),
+        'Panel',
+      );
+    }
     utils.book_append_sheet(workbook, worksheet, 'Sheet1');
     const excelBuffer = write(workbook, { bookType: 'xlsx', type: 'array' });
     const blob = new Blob([excelBuffer], { type: 'application/octet-stream' });
@@ -33,6 +45,14 @@ const ExportForm: FC<{ data?: Root }> = ({ data }) => {
           className={styles.input}
           value={fileName}
           onChange={(e) => setFileName(e.target.value)}
+        />
+      </label>
+      <label>
+        Panel
+        <input
+          type={'checkbox'}
+          checked={panel}
+          onChange={() => setPanel(!panel)}
         />
       </label>
       <GenericButton
