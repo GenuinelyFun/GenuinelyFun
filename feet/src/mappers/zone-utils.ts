aecf6719c65847cad185f9ad19cbe91cb26b31b7
NghiNg <60237320+NghiNg@users.noreply.github.com>
2024-10-05 18:42:14 +0200

    feat: Add zone sheet functionality (#34)

---
+++ feet/src/mappers/zone-utils.ts
@@ -0,0 +1,35 @@
+import { Panel, Zone } from '../interfaces/jsonDataInterface';
+
+export const mapZonesToExcel = (zone: Zone, panel: number) => {
+  return {
+    'Panel Number': panel,
+    Zone: zone.number,
+    Description: zone.description || 'N/A',
+  };
+};
+
+export const mapPanelsWithZones = (
+  panels: Panel[],
+  zones: Zone[],
+): {
+  'Panel Number': number;
+  Zone: number;
+  Description: string;
+}[] => {
+  const mappedPanelZones: {
+    'Panel Number': number;
+    Zone: number;
+    Description: string;
+  }[] = [];
+
+  panels.map((panel) => {
+    const firstZone = panel.first_zone;
+    const amountOfZones = panel.number_of_zones;
+
+    for (let i = firstZone - 1; i < firstZone + amountOfZones - 1; i++) {
+      mappedPanelZones.push(mapZonesToExcel(zones[i], panel.number));
+    }
+  });
+
+  return mappedPanelZones;
+};
