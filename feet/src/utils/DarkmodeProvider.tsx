e51ddd2ace83c6839e009d09143d55a949398bed
NghiNg <60237320+NghiNg@users.noreply.github.com>
2024-10-06 11:42:59 +0200

    feat: fix first page load of darkmode (#35)

--- feet/src/utils/DarkmodeProvider.tsx
+++ feet/src/utils/DarkmodeProvider.tsx
@@ -1,4 +1,4 @@
-import { createContext, FC, PropsWithChildren } from 'react';
+import { createContext, FC, PropsWithChildren, useEffect } from 'react';
 import useLocalStorage from 'use-local-storage';
 import { useContextOrThrow } from './context-utils';
 
@@ -26,6 +26,13 @@ export const DarkmodeProvider: FC<PropsWithChildren> = ({ children }) => {
     'theme',
     defaultLight ? Darkmode.Light : Darkmode.Dark,
   );
+
+  useEffect(() => {
+    if (!defaultLight) {
+      setTheme(Darkmode.Dark);
+      document.documentElement.setAttribute('data-theme', Darkmode.Dark);
+    }
+  }, []);
 
   const toggleTheme = () => {
     if (theme === Darkmode.Light) {
