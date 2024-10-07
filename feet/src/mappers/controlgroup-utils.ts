2ff6fcd3a619cb327c8a0e2148dfd00f18633492
Ng <Nguyenbdnghi@gmail.com>
2024-10-07 20:54:15 +0200

    feat: add control group excel sheet WARNING: Something is still not quite right here.

---
+++ feet/src/mappers/controlgroup-utils.ts
@@ -0,0 +1,191 @@
+import { Panel } from '../interfaces/jsonDataInterface';
+import { forEachDeviceInLoopControllers } from './loop-utils';
+
+export const mapControlGroupsToExcel = (panels: Panel[]) => {
+  const inputs: Record<string, any>[] = [];
+  const loop_outputs: Record<string, any>[] = [];
+  const board_outputs: Record<string, any>[] = [];
+  const structured_data: Record<string, any>[] = [];
+  const loveBabe = 'I will always love you, from your silly man';
+
+  panels.forEach((panel) => {
+    panel.input_output_units.forEach((board) => {
+      board.clean_contact_inputs?.forEach((input) => {
+        inputs.push({
+          'Control Group':
+            input.control_group_A ||
+            input.control_group_B ||
+            input.control_group_B2,
+          Address: `Panel ${panel.number} - ${board.type} ${board.number} - Input ${input.number}`,
+          'Device ID': null,
+          Zone: null,
+          Description: input.description,
+          'Device Type': null,
+          'Protocol Type': null,
+        });
+      });
+
+      board.clean_contact_outputs
+        .concat(board.monitored_outputs || [])
+        ?.forEach((output) => {
+          const { output_control } = output;
+          if (output_control.control_groups.length > 0) {
+            output_control.control_groups.forEach((control) => {
+              board_outputs.push({
+                'Control Groups': control,
+                'Control Groups Address': `Panel ${panel.number} - ${board.type} ${board.number} - Output ${output.number}`,
+                'Control Groups Device ID': null,
+                'Control Groups Zone': null,
+                'Control Groups Description': output.description,
+                'Control Groups Device Type': null,
+                'Control Groups Protocol Type': null,
+              });
+            });
+          } else {
+            board_outputs.push({
+              'Control Groups':
+                output.output_control.control === 'Local control'
+                  ? 'Control groups local control'
+                  : 'Control groups general control',
+              'Control Groups Address': `Panel ${panel.number} - ${board.type} ${board.number} - Output ${output.number}`,
+              'Control Groups Device ID': null,
+              'Control Groups Zone': null,
+              'Control Groups Description': output.description,
+              'Control Groups Device Type': null,
+              'Control Groups Protocol Type': null,
+            });
+          }
+        });
+    });
+  });
+  forEachDeviceInLoopControllers(panels, (panel, loop, device) => {
+    const address =
+      String(loop.number).padStart(3, '0') +
+      '.' +
+      String(device.number).padStart(3, '0');
+    const control_group_ABB2 = [
+      device.control_group_A,
+      device.control_group_B,
+      device.control_group_B2,
+    ].filter((el) => !!el);
+    const { output_control } = device;
+    const { control_groups } = output_control || {};
+    const { control } = output_control || {};
+    if (control_group_ABB2.length > 0) {
+      control_group_ABB2.forEach((control_group) => {
+        if (device.description === 'ROM 03G01.903 INTERN KORRIDOR') {
+          console.log(device);
+        }
+        inputs.push({
+          'Control Group': control_group,
+          Address: address,
+          'Device ID': device.device_id,
+          Zone: device.zone,
+          Description: device.description,
+          'Device Type': device.type,
+          'Protocol Type': device.protocol_type,
+        });
+      });
+    } else if (control_groups || control) {
+      if (control === 'General control' && control_groups?.length === 0) {
+        loop_outputs.push({
+          'Control Groups': 'Control groups general control',
+          'Control Groups Address': address,
+          'Control Groups Device ID': device.device_id,
+          'Control Groups Zone': device.zone,
+          'Control Groups Description': device.description,
+          'Control Groups Device Type': device.type,
+          'Control Groups Protocol Type': device.protocol_type,
+        });
+      } else if (control === 'Local control' && control_groups?.length === 0) {
+        loop_outputs.push({
+          'Control Groups': 'Control groups local control',
+          'Control Groups Address': address,
+          'Control Groups Device ID': device.device_id,
+          'Control Groups Zone': device.zone,
+          'Control Groups Description': device.description,
+          'Control Groups Device Type': device.type,
+          'Control Groups Protocol Type': device.protocol_type,
+        });
+      } else if (control_groups !== undefined && control_groups.length > 0) {
+        control_groups.forEach((control_group) => {
+          loop_outputs.push({
+            'Control Groups': control_group,
+            'Control Groups Address': address,
+            'Control Groups Device ID': device.device_id,
+            'Control Groups Zone': device.zone,
+            'Control Groups Description': device.description,
+            'Control Groups Device Type': device.type,
+            'Control Groups Protocol Type': device.protocol_type,
+          });
+        });
+      }
+    }
+  });
+  const panel_inputs = inputs.filter((input) =>
+    input['Address'].includes('Panel'),
+  );
+  const number_inputs = inputs.filter(
+    (input) => !input['Address'].includes('Panel'),
+  );
+
+  const inputsToExcel = panel_inputs.concat(number_inputs).sort((a, b) => {
+    if (a['Control Group'] === undefined) {
+      return 1000;
+    }
+    if (b['Control Group'] === undefined) {
+      return -1000;
+    }
+    return Number(a['Control Group']) - Number(b['Control Group']);
+  });
+  const outputToExcel = board_outputs.concat(loop_outputs).sort((a, b) => {
+    if (!isNaN(a['Control Groups']) && !isNaN(b['Control Groups'])) {
+      return a['Control Groups'] - b['Control Groups'];
+    }
+    if (isNaN(a['Control Groups']) && !isNaN(b['Control Groups'])) {
+      return 1;
+    }
+    if (!isNaN(a['Control Groups']) && isNaN(b['Control Groups'])) {
+      return -1;
+    }
+    return String(a['Control Groups']).localeCompare(
+      String(b['Control Groups']),
+    );
+  });
+
+  const keys = [
+    ...inputsToExcel.map((input) => input['Control Group']),
+    ...outputToExcel.map((output) => output['Control Groups']),
+  ]
+    .filter((value, index, array) => array.indexOf(value) === index)
+    .sort((a, b) => {
+      if (!isNaN(a) && !isNaN(b)) {
+        return a - b;
+      }
+      if (!isNaN(a) && isNaN(b)) {
+        return -1;
+      }
+      if (isNaN(a) && !isNaN(b)) {
+        return 1;
+      }
+      return String(a).localeCompare(b);
+    });
+  keys.forEach((key) => {
+    const inputs = inputsToExcel.filter(
+      (input) => input['Control Group'] === key,
+    );
+    const outputs = outputToExcel.filter(
+      (output) => output['Control Groups'] === key,
+    );
+    for (let i = 0; i < Math.max(inputs.length, outputs.length); i++) {
+      if (inputs[i] !== undefined && outputs[i] !== undefined) {
+        structured_data.push({ ...inputs[i], ...outputs[i] });
+      } else if (inputs[i] !== undefined && outputs[i] === undefined) {
+        structured_data.push(inputs[i]);
+      } else if (inputs[i] === undefined && outputs[i] !== undefined) {
+        structured_data.push(outputs[i]);
+      }
+    }
+  });
+  return structured_data;
+};
--- feet/src/pages/feetpage/ExportForm.tsx
+++ feet/src/pages/feetpage/ExportForm.tsx
@@ -1,6 +1,7 @@
-import { FC, FormEventHandler, useState } from 'react';
+import { CSSProperties, FC, FormEventHandler, useState } from 'react';
 import { utils, write } from 'xlsx';
 import FileSaver from 'file-saver';
+import PacmanLoader from 'react-spinners/PacmanLoader';
 
 import { useLanguageContext } from '../../utils/LanguageProvider';
 import GenericButton from '../../components/GenericButton';
@@ -13,23 +14,30 @@ import { mapBoardToExcel } from '../../mappers/board-utils';
 import { mapLoopAddressToExcel } from '../../mappers/address-utils';
 import { mapToIOReportToExcel } from '../../mappers/io-report-utils';
 import styles from './ExportForm.module.less';
+import { mapControlGroupsToExcel } from '../../mappers/controlgroup-utils';
 
-const ExportForm: FC<{ data?: Root }> = ({ data }) => {
+const ExportForm: FC<{ data?: Root; filename?: string }> = ({
+  data,
+  filename,
+}) => {
   const { translate } = useLanguageContext();
   const [fileName, setFileName] = useState<string>(
-    translate('export.filename.placeholder'),
+    filename || translate('export.filename.placeholder'),
   );
-  const [panel, setPanel] = useState<boolean>(false);
-  const [zone, setZone] = useState<boolean>(false);
-  const [loop, setLoop] = useState<boolean>(false);
-  const [board, setBoard] = useState<boolean>(false);
-  const [address, setAddress] = useState<boolean>(false);
-  const [report, setReport] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState(true);
+  const [panel, setPanel] = useState<boolean>(true);
+  const [zone, setZone] = useState<boolean>(true);
+  const [loop, setLoop] = useState<boolean>(true);
+  const [board, setBoard] = useState<boolean>(true);
+  const [address, setAddress] = useState<boolean>(true);
+  const [report, setReport] = useState<boolean>(true);
+  const [control, setControl] = useState<boolean>(true);
 
   const exportToExcel: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
     if (data === undefined) return;
-
+    setLoading(true);
     const workbook = utils.book_new();
 
     const panels = data.system.panels;
@@ -83,118 +91,193 @@ const ExportForm: FC<{ data?: Root }> = ({ data }) => {
         'IOReport',
       );
     }
+    if (control) {
+      utils.book_append_sheet(
+        workbook,
+        utils.json_to_sheet(mapControlGroupsToExcel(panels)),
+        'Control Groups Detailed',
+      );
+    }
 
     const excelBuffer = write(workbook, { bookType: 'xlsx', type: 'array' });
     const blob = new Blob([excelBuffer], { type: 'application/octet-stream' });
+    setLoading(false);
     FileSaver.saveAs(blob, `${fileName}.xlsx`);
   };
 
+  const toggleSelectAll = () => {
+    const isAllSelected =
+      panel && zone && loop && board && address && report && control;
+
+    if (isAllSelected) {
+      setPanel(false);
+      setZone(false);
+      setLoop(false);
+      setBoard(false);
+      setAddress(false);
+      setReport(false);
+      setControl(false);
+    } else {
+      setPanel(true);
+      setZone(true);
+      setLoop(true);
+      setBoard(true);
+      setAddress(true);
+      setReport(true);
+      setControl(true);
+    }
+  };
+
   return (
-    <form className={styles.container} onSubmit={exportToExcel}>
-      <label>
-        {translate('export.filename.label')}
-        <input
-          className={styles.input}
-          value={fileName}
-          onChange={(e) => setFileName(e.target.value)}
-        />
-      </label>
-      <div className={styles.checkboxContainer}>
-        <InfoBox
-          message={translate('export.panel.infobox.description')}
-          header={translate('export.panel.infobox.title')}
-          altText="Help Icon"
-        />
-        <label className={styles.label}>
-          {translate('export.panel.checkbox.label')}
+    <>
+      <form className={styles.container} onSubmit={exportToExcel}>
+        <label>
+          {translate('export.filename.label')}
           <input
-            type={'checkbox'}
-            checked={panel}
-            onChange={() => setPanel(!panel)}
+            className={styles.input}
+            value={fileName}
+            onChange={(e) => setFileName(e.target.value)}
           />
         </label>
-      </div>
-      <div className={styles.checkboxContainer}>
-        <InfoBox
-          message={translate('export.zone.infobox.description')}
-          header={translate('export.zone.infobox.title')}
-          altText="Help Icon"
-        />
-        <label className={styles.label}>
-          {translate('export.zone.checkbox.label')}
+        <label>
           <input
             type={'checkbox'}
-            checked={zone}
-            onChange={() => setZone(!zone)}
+            checked={
+              panel && zone && loop && board && address && report && control
+            }
+            onChange={toggleSelectAll}
           />
+          Select all
         </label>
-      </div>
-      <div className={styles.checkboxContainer}>
-        <InfoBox
-          message={translate('infobox-checkbox-loop-text')}
-          header={translate('infobox-checkbox-loop-title')}
-          altText="Help Icon"
+        <div className={styles.checkboxContainer}>
+          <InfoBox
+            message={translate('export.panel.infobox.description')}
+            header={translate('export.panel.infobox.title')}
+            altText="Help Icon"
+          />
+          <label className={styles.label}>
+            {translate('export.panel.checkbox.label')}
+            <input
+              type={'checkbox'}
+              checked={panel}
+              onChange={() => setPanel(!panel)}
+            />
+          </label>
+        </div>
+        <div className={styles.checkboxContainer}>
+          <InfoBox
+            message={translate('export.zone.infobox.description')}
+            header={translate('export.zone.infobox.title')}
+            altText="Help Icon"
+          />
+          <label className={styles.label}>
+            {translate('export.zone.checkbox.label')}
+            <input
+              type={'checkbox'}
+              checked={zone}
+              onChange={() => setZone(!zone)}
+            />
+          </label>
+        </div>
+        <div className={styles.checkboxContainer}>
+          <InfoBox
+            message={translate('export.loop.infobox.description')}
+            header={translate('export.loop.infobox.title')}
+            altText="Help Icon"
+          />
+          <label className={styles.label}>
+            {translate('export.loop.checkbox.label')}
+            <input
+              type={'checkbox'}
+              checked={loop}
+              onChange={() => setLoop(!loop)}
+            />
+          </label>
+        </div>
+        <div className={styles.checkboxContainer}>
+          <InfoBox
+            message={translate('export.board.infobox.description')}
+            header={translate('export.board.infobox.title')}
+            altText="Help Icon"
+          />
+          <label className={styles.label}>
+            {translate('export.board.checkbox.label')}
+            <input
+              type={'checkbox'}
+              checked={board}
+              onChange={() => setBoard(!board)}
+            />
+          </label>
+        </div>
+        <div className={styles.checkboxContainer}>
+          <InfoBox
+            message={translate('export.address.infobox.description')}
+            header={translate('export.address.infobox.title')}
+            altText="Help Icon"
+          />
+          <label className={styles.label}>
+            {translate('export.address.checkbox.label')}
+            <input
+              type={'checkbox'}
+              checked={address}
+              onChange={() => setAddress(!address)}
+            />
+          </label>
+        </div>
+        <div className={styles.checkboxContainer}>
+          <InfoBox
+            message={translate('export.report.infobox.description')}
+            header={translate('export.report.infobox.title')}
+            altText="Help Icon"
+          />
+          <label className={styles.label}>
+            {translate('export.report.checkbox.label')}
+            <input
+              type={'checkbox'}
+              checked={report}
+              onChange={() => setReport(!report)}
+            />
+          </label>
+        </div>
+        <div className={styles.checkboxContainer}>
+          <InfoBox
+            message={translate('export.control.infobox.description')}
+            header={translate('export.control.infobox.title')}
+            altText="Help Icon"
+          />
+          <label className={styles.label}>
+            {translate('export.control.checkbox.label')}
+            <input
+              type={'checkbox'}
+              checked={control}
+              onChange={() => setControl(!control)}
+            />
+          </label>
+        </div>
+        <GenericButton
+          disabled={data === undefined}
+          buttonText={translate('export.download.button')}
         />
-        <label className={styles.label}>
-          {translate('export.checkbox.loop')}
-          <input
-            type={'checkbox'}
-            checked={loop}
-            onChange={() => setLoop(!loop)}
-          />
-        </label>
-      </div>
-      <div className={styles.checkboxContainer}>
-        <InfoBox
-          message={translate('infobox-checkbox-board-text')}
-          header={translate('infobox-checkbox-board-title')}
-          altText="Help Icon"
-        />
-        <label className={styles.label}>
-          {translate('export.checkbox.board')}
-          <input
-            type={'checkbox'}
-            checked={board}
-            onChange={() => setBoard(!board)}
-          />
-        </label>
-      </div>
-      <div className={styles.checkboxContainer}>
-        <InfoBox
-          message={translate('infobox-checkbox-address-text')}
-          header={translate('infobox-checkbox-address-title')}
-          altText="Help Icon"
-        />
-        <label className={styles.label}>
-          {translate('export.checkbox.address')}
-          <input
-            type={'checkbox'}
-            checked={address}
-            onChange={() => setAddress(!address)}
-          />
-        </label>
-      </div>
-      <div className={styles.checkboxContainer}>
-        <InfoBox
-          message={translate('infobox-checkbox-report-text')}
-          header={translate('infobox-checkbox-report-title')}
-          altText="Help Icon"
-        />
-        <label className={styles.label}>
-          {translate('export.checkbox.report')}
-          <input
-            type={'checkbox'}
-            checked={report}
-            onChange={() => setReport(!report)}
-          />
-        </label>
-      </div>
-      <GenericButton
-        disabled={data === undefined}
-        buttonText={translate('export.download.button')}
+      </form>
+      {error && <p>Something went wrong</p>}
+      <PacmanLoader
+        color={'#00573f'}
+        loading={!loading}
+        size={24}
+        cssOverride={spinnerStyling}
+        aria-label="Loading Spinner"
+        data-testid="loader"
       />
-    </form>
+      <p>Vennligst vent mens vi.. no morsomt no</p>
+    </>
   );
 };
 
 export default ExportForm;
+
+const spinnerStyling: CSSProperties = {
+  display: 'block',
+  margin: '24px auto',
+  height: '24px',
+  left: '-16px',
+};
