f2d78fd71957eca01774785bfe4e50da17ae2b36
Friftycode <arthur.leonard.thomassen@gmail.com>
2024-10-17 12:37:22 +0200

    fix: My wonderful wife fixed the control group report

--- feet/src/pages/feetpage/ImportForm.tsx
+++ feet/src/pages/feetpage/ImportForm.tsx
@@ -14,7 +14,6 @@ import { useDataContext } from '../../utils/DataProvider';
 import GenericButton from '../../components/GenericButton';
 import styles from './ImportForm.module.less';
 
-
 const FIRE_EXPERT_VERSION = '24.5';
 
 const ImportForm: FC = () => {
