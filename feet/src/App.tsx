43d40d2d0ebdeb800a35b046ae97f7ceb7203efa
Friftycode <arthur.leonard.thomassen@gmail.com>
2024-11-05 18:20:51 +0100

    chore: Update routes to exist in a variable and add two new pages

--- feet/src/App.tsx
+++ feet/src/App.tsx
@@ -7,6 +7,17 @@ import NotFoundPage from './pages/notfoundpage/NotFoundPage';
 import FeetPage from './pages/feetpage/FeetPage';
 import DropZonePage from './pages/journalpage/DropZonePage';
 import TechBestPracticePage from './pages/journalpage/TechBestPracticePage';
+import ArthurPage from './pages/portfolio/ArthurPage';
+import NghiPage from './pages/portfolio/NghiPage';
+
+export const routePaths = {
+  home: '/',
+  dropzone: '/dropzone',
+  techBestPractice: '/tech-best-practice',
+  feet: '/feet',
+  arthur: '/arthur',
+  nghi: '/nghi',
+};
 
 const App = (): JSX.Element => {
   return (
@@ -14,11 +25,16 @@ const App = (): JSX.Element => {
       <Toaster position="top-center" reverseOrder={false} />
       <Header />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path={routePaths.home} element={<HomePage />} />
         <Route path="*" element={<NotFoundPage />} />
-        <Route path="/dropzone" element={<DropZonePage />} />
-        <Route path="/tech-best-practice" element={<TechBestPracticePage />} />
-        <Route path="/feet" element={<FeetPage />} />
+        <Route path={routePaths.dropzone} element={<DropZonePage />} />
+        <Route
+          path={routePaths.techBestPractice}
+          element={<TechBestPracticePage />}
+        />
+        <Route path={routePaths.feet} element={<FeetPage />} />
+        <Route path={routePaths.arthur} element={<ArthurPage />} />
+        <Route path={routePaths.nghi} element={<NghiPage />} />
       </Routes>
       <Footer />
     </>
