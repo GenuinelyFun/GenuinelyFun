7024467bd95e13a0492c24fe708e78992816bb2a
Friftycode <arthur.leonard.thomassen@gmail.com>
2024-12-28 14:00:19 +0100

    chore: Made so that we only get text "E-mail us" on screen size <= 1040.

--- feet/src/components/Footer.tsx
+++ feet/src/components/Footer.tsx
@@ -7,46 +7,46 @@ import { useMobileSizes } from '../utils/useMobileSizes';
 
 const Footer: FC = () => {
   const { translate } = useLanguageContext();
-  const { isDesktop } = useMobileSizes();
+  const { isNotDesktop } = useMobileSizes();
 
   return (
-    <footer className={styles.footer}>
-      <div className={styles.footerContainer}>
-        <div className={styles.column}>
-          <h5>GenuinelyFun</h5>
-          <a href={'mailto:post@genuinelyfun.com'}>
-            {translate('email')}: post@genuinelyfun.com
-          </a>
-          <p className={styles.allRightsReserved}>© 2024 GenuinelyFun.</p>
+      <footer className={styles.footer}>
+        <div className={styles.footerContainer}>
+          <div className={styles.column}>
+            <h5>GenuinelyFun</h5>
+            <a href={'mailto:post@genuinelyfun.com'}>
+              {isNotDesktop? translate('email-us') : `${translate('email')}: post@genuinelyfun.com`}
+            </a>
+            <p className={styles.allRightsReserved}>© 2024 GenuinelyFun.</p>
+          </div>
+          <div className={styles.column}>
+            <h5>Nghi</h5>
+            <ul className={styles.linkList}>
+              {Object.values(nghiLinks).map((link) => (
+                  <li key={link.name} className={styles.link}>
+                    <a href={link.url}>
+                      {link.icon}
+                      {isNotDesktop && link.name}
+                    </a>
+                  </li>
+              ))}
+            </ul>
+          </div>
+          <div className={styles.column}>
+            <h5>Arthur</h5>
+            <ul className={styles.linkList}>
+              {Object.values(arthurLinks).map((link) => (
+                  <li key={link.name} className={styles.link}>
+                    <a href={link.url}>
+                      {link.icon}
+                      {isNotDesktop && link.name}
+                    </a>
+                  </li>
+              ))}
+            </ul>
+          </div>
         </div>
-        <div className={styles.column}>
-          <h5>Nghi</h5>
-          <ul className={styles.linkList}>
-            {Object.values(nghiLinks).map((link) => (
-              <li key={link.name} className={styles.link}>
-                <a href={link.url}>
-                  {link.icon}
-                  {isDesktop && link.name}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className={styles.column}>
-          <h5>Arthur</h5>
-          <ul className={styles.linkList}>
-            {Object.values(arthurLinks).map((link) => (
-              <li key={link.name} className={styles.link}>
-                <a href={link.url}>
-                  {link.icon}
-                  {isDesktop && link.name}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </footer>
+      </footer>
   );
 };
 
