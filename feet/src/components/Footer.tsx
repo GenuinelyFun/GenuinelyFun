d9b44fc5c38084449c8f4c5b365010e59d59e5ef
Friftycode <arthur.leonard.thomassen@gmail.com>
2025-01-10 06:23:13 +0100

    chore: Updated footer and genericTexts.json.

--- feet/src/components/Footer.tsx
+++ feet/src/components/Footer.tsx
@@ -1,45 +1,49 @@
-import { FC } from 'react';
-import { useLanguageContext } from '../utils/LanguageProvider';
+import {FC} from 'react';
+import {useLanguageContext} from '../utils/LanguageProvider';
+import {useMobileSizes} from '../utils/useMobileSizes';
+import {nghiLinks} from '../utils/nghi-utils';
+import {arthurLinks} from '../utils/arthur-utils';
 import styles from './Footer.module.less';
-import { nghiLinks } from '../utils/nghi-utils';
-import { arthurLinks } from '../utils/arthur-utils';
-import { useMobileSizes } from '../utils/useMobileSizes';
 
 const Footer: FC = () => {
   const { translate } = useLanguageContext();
-  const { isNotDesktop } = useMobileSizes();
+  const { isNotDesktop, isDesktop } = useMobileSizes();
 
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContainer}>
         <div className={styles.column}>
-          <h5>GenuinelyFun</h5>
-          <a href={'mailto:post@genuinelyfun.com'}>
-            {isNotDesktop
-              ? translate('email-us')
-              : `${translate('email')}: post@genuinelyfun.com`}
+          <h5>{translate('genuinelyfun.name')}</h5>
+          <a href={'mailto:' + translate('genuinelyfun.email')}>
+            {isNotDesktop ? translate('email-us') : `${translate('email')}: ${translate('genuinelyfun.email')}`}
           </a>
-          <p className={styles.allRightsReserved}>© 2024 GenuinelyFun.</p>
+          <p className={styles.allRightsReserved}>
+            {translate('genuinelyfun.all_rights_reserved') +
+                ' ' +
+                translate('genuinelyfun.name')}
+          </p>
         </div>
         <div className={styles.column}>
-          <h5>Nghi</h5>
+          <h5>{translate('nghi.firstname')}</h5>
           <ul className={styles.linkList}>
             {Object.values(nghiLinks).map((link) => (
               <li key={link.name} className={styles.link}>
                 <a href={link.url} target="_blank" rel="noopener noreferrer">
-                  {isNotDesktop ? link.icon : <>{link.icon} {link.name}</>}
+                  {link.icon}
+                  {isDesktop && link.name}
                 </a>
               </li>
             ))}
           </ul>
         </div>
         <div className={styles.column}>
-          <h5>Arthur</h5>
+          <h5>{translate('arthur.firstname')}</h5>
           <ul className={styles.linkList}>
             {Object.values(arthurLinks).map((link) => (
               <li key={link.name} className={styles.link}>
                 <a href={link.url} target="_blank" rel="noopener noreferrer">
-                  {isNotDesktop ? link.icon : <>{link.icon} {link.name}</>}
+                  {link.icon}
+                  {isDesktop && link.name}
                 </a>
               </li>
             ))}
