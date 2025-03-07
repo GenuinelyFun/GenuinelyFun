bd53a079ce0d3f27833e90a9e069c3be2b43ffcb
NghiNg <nghi.nguyen@posten.no>
2025-03-07 15:23:20 +0100

    vite base config runs correctly

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,26 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import reactSWC from '@vitejs/plugin-react-swc'
-import svgr from 'vite-plugin-svgr'
+import react from '@vitejs/plugin-react';
+import reactSWC from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
+import PrettyModuleClassnames from 'vite-plugin-pretty-module-classnames';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), reactSWC(), svgr()],
+  plugins: [
+    react(),
+    reactSWC(),
+    svgr(),
+    checker({
+      typescript: true,
+    }),
+    PrettyModuleClassnames(),
+  ],
   resolve: { preserveSymlinks: true },
   css: {
     modules: {
       localsConvention: 'camelCase',
-      generateScopedName:
-          command === 'build'
-              ? '[hash:base64:5]'
-              : '[name]__[local]__[hash:base64:5]',
     },
     preprocessorOptions: {
       less: {
-        math: "always",
+        math: 'always',
         relativeUrls: true,
-        javascriptEnabled: true
+        javascriptEnabled: true,
       },
-    }
-  }
-})
+    },
+  },
+});
