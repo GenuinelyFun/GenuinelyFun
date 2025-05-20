b902709c9c91613ee972514b86e050ea8c80aee0
NghiNg <nghi.nguyen@posten.no>
2025-05-20 14:01:21 +0200

    fix(fweet): sort Loop.circuitNo and UnitNo

--- frontend/src/projects/fweet/loop-utils.ts
+++ frontend/src/projects/fweet/loop-utils.ts
@@ -5,9 +5,18 @@ import { AddrUnitType } from './verify-utils.ts';
 
 export const loopMapper = (db: Database): { [p: string]: SheetValueType }[] => {
   const results = db.exec('SELECT * FROM AddrUnit');
-  return results[0].values.map((row) => {
-    return mapLoop(results[0].columns, row as SheetValueType[]);
-  });
+  const mappedResults = Object.groupBy(
+    results[0].values.map((row) => {
+      return mapLoop(results[0].columns, row as SheetValueType[]);
+    }),
+    (row) => row['CircuitNo'] as string
+  );
+
+  return Object.values(mappedResults)
+    .map((group) =>
+      group?.sort((a, b) => Number(a['UnitNo']) - Number(b['UnitNo']))
+    )
+    .flat() as { [p: string]: SheetValueType }[];
 };
 
 const mapLoop = (columns: string[], row: SheetValueType[]) => {
