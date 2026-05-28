3bc97131b0f4357a60b3e5ec86bf094f1dedf38e
Friftycode <arthur.leonard.thomassen@gmail.com>
2026-05-28 10:37:17 +0200

    fix: align banner tablet margin and show burger on tablet

--- frontend/src/utils/useMobileSizes.tsx
+++ frontend/src/utils/useMobileSizes.tsx
@@ -3,7 +3,7 @@ import { useWindowSize } from 'usehooks-ts';
 export const useMobileSizes = () => {
   const { width } = useWindowSize();
   return {
-    isMobile: width <= 768,
+    isMobile: width <= 1040,
     isTablet: width <= 1040 && width > 768,
     isDesktop: width > 1040,
     isNotDesktop: width <= 1040,
